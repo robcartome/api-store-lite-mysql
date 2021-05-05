78ed361b34e4b327f6588d34bc39e80a3234ea08
Robert <robert.tm2018@gmail.com>
2021-05-05 10:27:08 -0500

    Maked Models and Migrations for Create Tables

---
+++ db/migrate/20210505152516_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :url_image
+      t.float :price
+      t.integer :discount
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
