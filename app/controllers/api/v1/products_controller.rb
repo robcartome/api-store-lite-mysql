b49cc1287bff0afa5af2c5fcddd327afe7efa6ac
Robert <robert.tm2018@gmail.com>
2021-05-05 22:36:23 -0500

    Maked Function in controller Products to manager routes and show products

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class ProductsController < ApplicationController
+      before_action :set_product, only: %i[show update destroy]
+
+      # GET /products
+      def index
+        products = Product.all
+        render json: { status: 'OK', data: products }, status: :ok
+      end
+
+      # GET /categories/category_id/products
+      def show_by_category
+        category = Category.find(params[:category_id])
+        products = category.products
+        render json: { status: 'OK', data: products }, status: :ok
+      end
+
+      # GET /products/{id}
+      def show
+        render json: { status: 'OK', data: @product }, status: 200
+      end
+
+      def create; end
+
+      private
+
+      # Use callbacks to share common setup or constraints between actions.
+      def set_product
+        @product = Product.find(params[:id])
+      end
+
+      # Only allow a trusted parameter "white list" through.
+      def product_params
+        params.require(:product).permit(:name, :price, :discount, :category_id)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace 'api' do
     namespace 'v1' do
-      resources :categories
+      resources :categories do
+        get 'products', to: 'products#show_by_category'
+      end
       resources :products
     end
   end
